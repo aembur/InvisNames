112852f30fc73c61d9c51cbc0aec4963c28b5f61
aembr <aembr@duck.com>
2023-03-30 22:31:48 +0100

    Add health rendering

---
+++ src/main/java/com/aembr/healthtags/HealthTags.java
@@ -0,0 +1,17 @@
+package com.aembr.healthtags;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@Mod(modid = HealthTags.MOD_ID)
+@SideOnly(Side.CLIENT)
+public class HealthTags {
+	public static final String MOD_ID = "healthtags";
+	
+	@Mod.EventHandler
+	public void onPreInit(FMLPreInitializationEvent event) {
+		System.out.println("Hello world!");
+	}
+}
---
+++ src/main/java/com/aembr/healthtags/mixin/RenderLivingMixin.java
@@ -0,0 +1,48 @@
+package com.aembr.healthtags.mixin;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.player.EntityPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.entity.RenderLivingBase;
+import net.minecraft.entity.EntityLivingBase;
+
+@Mixin(RenderLivingBase.class)
+public abstract class RenderLivingMixin<T extends EntityLivingBase> extends Render<T> {
+
+    public RenderLivingMixin(RenderManager renderManager) {
+        super(renderManager);
+    }
+
+    @Inject(method = "doRender(Lnet/minecraft/entity/EntityLivingBase;DDDFF)V", at = @At("HEAD"))
+    public void doRender(EntityLivingBase entity, double x, double y, double z, float entityYaw, float partialTicks, CallbackInfo ci) {
+        if (entity instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer) entity;
+            int health = (int) Math.ceil(player.getHealth());
+            String healthStr = String.valueOf(health);
+
+            GlStateManager.pushMatrix();
+            GlStateManager.translate(x, y + entity.height + 0.8F, z);
+            GlStateManager.rotate(-this.renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
+            GlStateManager.rotate(this.renderManager.playerViewX, 1.0F, 0.0F, 0.0F);
+            GlStateManager.scale(-0.025F, -0.025F, 0.025F);
+            GlStateManager.disableLighting();
+            RenderHelper.disableStandardItemLighting();
+            GlStateManager.depthMask(false);
+            Minecraft.getMinecraft().fontRenderer.drawString(healthStr, -Minecraft.getMinecraft().fontRenderer.getStringWidth(healthStr) / 2, 0, 0xFFFFFF, false);
+            GlStateManager.depthMask(true);
+            RenderHelper.enableStandardItemLighting();
+            GlStateManager.enableLighting();
+            GlStateManager.popMatrix();
+        }
+    }
+}
+
+
